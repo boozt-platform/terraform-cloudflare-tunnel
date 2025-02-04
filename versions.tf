e0810fa5225126cd6ad2d8a018c719f6ee63fec6
Donatas Navidonskis <donnav@boozt.com>
2025-02-04 11:36:40 +0200

    feat: add Cloudflare Tunnel module

---
+++ versions.tf
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: Copyright Boozt Fashion, AB
+# SPDX-License-Identifier: MIT
+
+terraform {
+  required_version = ">= 1.10"
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "< 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.api_token
+}
